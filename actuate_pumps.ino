dde1df42df512622f7545d16c937af0665da9423
saudag <saudaghatge09@gmail.com>
2024-06-17 10:40:26 -0400

    arduino code updated

--- actuate_pumps.ino
+++ actuate_pumps.ino
@@ -1,6 +1,7 @@
-const int pumpPin1 = 4; // First pump is controlled by digital pin 11
-const int pumpPin2 = 2; // Second pump is controlled by digital pin 12
-const int pumpPin3 = 6;
+
+const int pumpPin1 = 4;
+const int pumpPin2 = 3;
+const int pumpPin3 = 2;
 void setup() {
   // Initialize the serial communication at 9600 bits per second:
   Serial.begin(9600);
@@ -11,9 +12,9 @@ void setup() {
   pinMode(pumpPin3, OUTPUT);
 
   // Ensure the pumps are off at startup:
-  digitalWrite(pumpPin1, LOW);
-  digitalWrite(pumpPin2, LOW);
-  digitalWrite(pumpPin3, LOW);
+  digitalWrite(pumpPin1, HIGH);
+  digitalWrite(pumpPin2, HIGH);
+  digitalWrite(pumpPin3, HIGH);
 }
 
 
@@ -28,9 +29,9 @@ void loop() {
 
     if (incomingByte == "pump1") {
 
-      digitalWrite(pumpPin1, HIGH); // Turn the first pump on
-      digitalWrite(pumpPin2, LOW);// Ensure the second pump is off
-      digitalWrite(pumpPin3, LOW);
+      digitalWrite(pumpPin1, LOW); // Turn the first pump on
+      digitalWrite(pumpPin2, HIGH);// Ensure the second pump is off
+      digitalWrite(pumpPin3, HIGH);
     
       Serial.write("pump on");
 
@@ -38,27 +39,27 @@ void loop() {
 
     else if (incomingByte == "pump2") {
 
-      digitalWrite(pumpPin1, LOW); // Ensure the first pump is off
-      digitalWrite(pumpPin2, HIGH); // Turn the second pump on
-      digitalWrite(pumpPin3, LOW);
-      // Serial.write("Led off");
-      Serial.write("pump on");
-
-    }
-    else if (incomingByte == "pump3") {
-
-      digitalWrite(pumpPin1, LOW); // Ensure the first pump is off
+      digitalWrite(pumpPin1, HIGH); // Ensure the first pump is off
       digitalWrite(pumpPin2, LOW); // Turn the second pump on
       digitalWrite(pumpPin3, HIGH);
       // Serial.write("Led off");
       Serial.write("pump on");
 
     }
+    else if (incomingByte == "pump3") {
+
+      digitalWrite(pumpPin1, HIGH); // Ensure the first pump is off
+      digitalWrite(pumpPin2, HIGH); // Turn the second pump on
+      digitalWrite(pumpPin3, LOW);
+      // Serial.write("Led off");
+      Serial.write("pump on");
+
+    }
 
     else if (incomingByte == "pumpsoff"){
-      digitalWrite(pumpPin1, LOW);
-      digitalWrite(pumpPin2, LOW);
-      digitalWrite(pumpPin3, LOW );
+      digitalWrite(pumpPin1, HIGH);
+      digitalWrite(pumpPin2, HIGH);
+      digitalWrite(pumpPin3, HIGH);
       Serial.write("PUMPS OFF");
 
     }
